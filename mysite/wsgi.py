6b17dc39abac45f418a5b0aa165ec8ed40f7ad7a
martin <martow17@gmail.com>
2016-04-03 12:42:05 -0800

    add blog files

---
+++ mysite/wsgi.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+path = '/home/marto-k/my-first-blog'  # use your own username here
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
